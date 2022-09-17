d08d61821d9553f47205bb838a3e83d5e34fdc4e
Twinrest <lipsoczech@gmail.com>
2022-09-17 09:18:28 +0200

    Je to test jestli mi to teď pustí

--- automatic-web-testing/Helper/Tests/LoginHelper.cs
+++ automatic-web-testing/Helper/Tests/LoginHelper.cs
@@ -20,25 +20,19 @@ namespace automatic_web_testing.Helper.Tests
             try
             {
                 var username = SearchHelper.WaitForElementById("Username", driver, 5, 250);
-        
                 Assert.IsNotNull(username, "Nenašel se input pro uživatelské jméno");
                 var password = driver.FindElement(By.Id("Password"));
                 Assert.IsNotNull(password, "Nenašel se input pro heslo míra ocas");
-        
                 username.SendKeys(UserHelper.CorrectUser.Email);
                 Assert.AreEqual(UserHelper.CorrectUser.Email, username.GetAttribute("value"), "Zadané hodnoty nesouhlasí s obdrženými");
                 password.SendKeys(UserHelper.CorrectUser.Password);
                 Assert.AreEqual(UserHelper.CorrectUser.Password, password.GetAttribute("value"), "Zadané hodnoty nesouhlasí s obdrženými");
-        
                 var prihlasitButton = driver.FindElement(By.Name("button"));
                 Assert.IsNotNull(prihlasitButton, "Nenašlo se tlačítko pro přihlášení");
                 prihlasitButton.Click();
-        
                 wait.Until(e => e.FindElements(By.TagName("button")).FirstOrDefault(f => f.Text == "Zobrazit všechny projekty"));
-        
                 var poPrihlaseni = driver.FindElements(By.TagName("button")).First(e => e.Text == "Zobrazit všechny projekty");
                 Assert.IsTrue(poPrihlaseni.Displayed, "Nenašel se element pro tlačítka (Zobrazit všechny projekty)");
-        
                 return true;
             }
             catch
@@ -57,20 +51,14 @@ namespace automatic_web_testing.Helper.Tests
             try
             {
                 var username = SearchHelper.WaitForElementById("Username", driver, 5, 250);
-        
                 var password = driver.FindElement(By.Id("Password"));
-        
                 username.SendKeys(UserHelper.CorrectUser.Email);
                 password.SendKeys(UserHelper.CorrectUser.Password);
-        
                 var prihlasitButton = driver.FindElement(By.Name("button"));
                 prihlasitButton.Click();
-        
                 wait.Until(e => e.FindElements(By.TagName("button")).FirstOrDefault(f => f.Text == "Zobrazit všechny projekty"));
-        
                 var poPrihlaseni = driver.FindElements(By.TagName("button")).First(e => e.Text == "Zobrazit všechny projekty");
                 Assert.NotNull(poPrihlaseni);
-        
                 return driver;
             }
             catch
